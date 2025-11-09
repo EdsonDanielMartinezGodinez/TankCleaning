4f96700eeb0575e8610d41aa6edf0c53aa656783
Edson Daniel Martinez Godinez <128575625+EdsonDanielMartinezGodinez@users.noreply.github.com>
2025-11-08 21:54:38 -0600

    Se craron compoenntes para empezar a dearrollar la estructura de la pagina, y se modificaron cosas de index.css y app.css

--- Front/tankcleaning/src/components/Tank.jsx
+++ Front/tankcleaning/src/components/Tank.jsx
@@ -2,6 +2,24 @@ import React, { Fragment } from 'react'
 
 export const Tank = () => {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+        <header>
+            <section className='maxwidth cabeza'>
+                <div>
+                    <img src="" alt="" />
+                    <h1>Grupo Goitsa</h1>
+                </div>
+                <nav>
+                    <a href=""><button>Inicio</button></a>
+                    <a href=""><button>Servicio</button></a>
+                    <a href=""><button>Nosotros</button></a>
+                    <a href=""><button>Contacto</button></a>
+                </nav>
+            </section>
+            <section></section>
+        </header>
+        <main></main>
+        <footer></footer>
+    </Fragment>
   )
 }
