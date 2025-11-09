9c5021ae5f01c840f1090e37ba4e14c51bc28995
Ethween <70765275+Ethgoin@users.noreply.github.com>
2025-11-08 23:36:48 -0600

    Cabecera añadida

--- Front/tankcleaning/src/components/Tank.jsx
+++ Front/tankcleaning/src/components/Tank.jsx
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react'
+import './Tank.css'
+import logo from './resources/logo.png'
 
 export const Tank = () => {
   return (
     <Fragment>
         <header>
             <section className='maxwidth cabeza'>
-                <div>
-                    <img src="" alt="" />
-                    <h1>Grupo Goitsa</h1>
+                <div className='isologo'>
+                    <img id='logoitsa' src={logo} alt="Goitsa"/>
+                    <p className='grupogoitsa'><b>Grupo Goitsa</b></p>
                 </div>
                 <nav>
                     <a href=""><button>Inicio</button></a>
@@ -16,7 +18,15 @@ export const Tank = () => {
                     <a href=""><button>Contacto</button></a>
                 </nav>
             </section>
-            <section></section>
+            <section>
+                <div>
+                    <h1>Cuidamos cada detalle con limpieza experta y segura en espacios confinados y plataformas offshore</h1>
+                </div>
+                <div className='cotiza&contacto'>
+                    <a href=""><button>cotiza ahora</button></a>
+                    <a href=""><button>Contacta ahora</button></a>
+                </div>
+            </section>
         </header>
         <main></main>
         <footer></footer>
