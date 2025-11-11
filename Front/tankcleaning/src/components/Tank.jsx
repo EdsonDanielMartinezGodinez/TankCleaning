9f1a34479a6ebd8b99a0730bfdb48ff5ae12da21
Edson Daniel Martinez Godinez <128575625+EdsonDanielMartinezGodinez@users.noreply.github.com>
2025-11-10 23:10:22 -0600

    Se pulio mas el diseño del Heroe y se agrego responsividad al diseño para que se adapte a diferntes pantallas

--- Front/tankcleaning/src/components/Tank.jsx
+++ Front/tankcleaning/src/components/Tank.jsx
@@ -1,34 +1,44 @@
 import React, { Fragment } from 'react'
 import './Tank.css'
 import logo from './resources/logo.png'
+import row from "./resources/row.png"
+import ejemplo from "./resources/example.mp4"
 
 export const Tank = () => {
   return (
     <Fragment>
         <header>
-            <section className='maxwidth cabeza'>
+            <section className='cabeza  maxwidth '>
+                <div className='overlay'></div>
+                <video className="videoBack" src={ejemplo} autoPlay={true} muted={true} loop={true}></video>
                 <div className='isologo'>
-                    <img id='logoitsa' src={logo} alt="Goitsa"/>
+                    <img id='logoitsa' src={logo} alt="Goitsa Logo"/>
                     <p className='grupogoitsa'><b>Grupo Goitsa</b></p>
                 </div>
-                <nav>
-                    <a href=""><button>Inicio</button></a>
-                    <a href=""><button>Servicio</button></a>
-                    <a href=""><button>Nosotros</button></a>
-                    <a href=""><button>Contacto</button></a>
+                <nav className='headNav'>
+                    <a href=""><button className='navBut'>Inicio</button></a>
+                    <a href=""><button className='navBut'>Servicio</button></a>
+                    <a href=""><button className='navBut'>Nosotros</button></a>
+                    <a href=""><button className='navBut'>Contacto</button></a>
                 </nav>
+                <img id="hamburguesa" src={row} alt="Icono de Menu" />
             </section>
-            <section>
+            <section className='cabezaBody maxwidth '>
                 <div>
-                    <h1>Cuidamos cada detalle con limpieza experta y segura en espacios confinados y plataformas offshore</h1>
+                    <h1>Cuidamos cada detalle con <b className='textYellow'>limpieza</b> experta y segura en <b className='textYellow'>espacios confinados</b> y <b className='textYellow'>plataformas offshore</b></h1>
                 </div>
-                <div className='cotiza&contacto'>
-                    <a href=""><button>cotiza ahora</button></a>
-                    <a href=""><button>Contacta ahora</button></a>
+                <div className='cotizacontacto'>
+                    <a href=""><button className='yellowBut'>Cotiza ahora</button></a>
+                    <a href=""><button className='invisibleBut'>Contacta ahora</button></a>
                 </div>
             </section>
         </header>
-        <main></main>
+        <main>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </main>
         <footer></footer>
     </Fragment>
   )
